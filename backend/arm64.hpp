93d4ebcf3d1fa9037242d462a2650ba38ff71b19
csjh <61282104+csjh@users.noreply.github.com>
2025-01-01 22:17:39 -0500

    make everything in arm64 template constexpr

--- backend/arm64.hpp
+++ backend/arm64.hpp
@@ -10,7 +10,7 @@
 namespace mitey {
 class Arm64 {
   public:
-    static std::array<uint8_t, sizeof(uint32_t) * 2> get_prelude() {
+    static constexpr std::array<uint8_t, sizeof(uint32_t) * 2> get_prelude() {
         auto arm = std::array<uint32_t, 2>{
             0xa9bf7bfd, // stp     x29, x30, [sp, #-0x10]!
             0x910003fd  // mov     x29, sp
@@ -19,7 +19,7 @@ class Arm64 {
         return u32_to_u8(arm);
     }
 
-    static std::array<uint8_t, sizeof(uint32_t) * 2> get_postlude() {
+    static constexpr std::array<uint8_t, sizeof(uint32_t) * 2> get_postlude() {
         auto arm = std::array<uint32_t, 2>{
             0xa8c17bfd, // ldp     x29, x30, [sp], #0x10
             0xd65f03c0  // ret
@@ -42,32 +42,38 @@ class Arm64 {
         return u32_to_u8(instructions);
     }
 
-    static std::array<uint8_t, sizeof(uint32_t) * 4> set_temp1(uint64_t value) {
+    static constexpr std::array<uint8_t, sizeof(uint32_t) * 4>
+    set_temp1(uint64_t value) {
         constexpr uint8_t x3 = 3;
         auto instructions = mov64(value, x3);
         return u32_to_u8(instructions);
     }
+    static constexpr size_t temp1_size = sizeof(set_temp1(0));
 
-    static std::array<uint8_t, sizeof(uint32_t) * 4> set_temp2(uint64_t value) {
+    static constexpr std::array<uint8_t, sizeof(uint32_t) * 4>
+    set_temp2(uint64_t value) {
         constexpr uint8_t x4 = 4;
         auto instructions = mov64(value, x4);
         return u32_to_u8(instructions);
     }
+    static constexpr size_t temp2_size = sizeof(set_temp1(0));
 
   private:
     template <size_t N>
-    static std::array<uint8_t, N * 4> u32_to_u8(std::array<uint32_t, N> u32) {
+    static constexpr std::array<uint8_t, N * 4>
+    u32_to_u8(std::array<uint32_t, N> u32) {
         return std::bit_cast<std::array<uint8_t, N * 4>>(u32);
     }
 
-    static std::array<uint32_t, 4> mov64(uint64_t value, uint8_t reg) {
+    static constexpr std::array<uint32_t, 4> mov64(uint64_t value,
+                                                   uint8_t reg) {
         return {mov16((value >> 0) & 0xffff, 0, reg),
                 mov16((value >> 16) & 0xffff, 1, reg),
                 mov16((value >> 32) & 0xffff, 2, reg),
                 mov16((value >> 48) & 0xffff, 3, reg)};
     }
 
-    static uint32_t mov16(uint16_t imm, uint8_t shift, uint8_t reg) {
+    static constexpr uint32_t mov16(uint16_t imm, uint8_t shift, uint8_t reg) {
         return (0b111100101 << 23) | (shift << 21) | (imm << 5) | reg;
     }
 };
